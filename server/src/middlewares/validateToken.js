897ad0b8e86bf40ca23dbf00a476d289b0ed996b
JairAlexey <alexeyrueda823@gmail.com>
2024-10-14 21:46:31 -0500

    Cambios en el validateToken

--- server/src/middlewares/validateToken.js
+++ server/src/middlewares/validateToken.js
@@ -1,8 +1,4 @@
-import jwt from 'jsonwebtoken';
-import { tokenSecret } from '../config.js';
-
 export const authRequired = (req, res, next) => {
-    console.log(req);
     const token = req.cookies ? req.cookies.token : null; // Verifica si req.cookies existe
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
@@ -11,7 +7,6 @@ export const authRequired = (req, res, next) => {
         if (err) return res.status(401).json({ message: 'Invalid token' });
         
         req.user = user; 
-        
         next();
     });
 };
