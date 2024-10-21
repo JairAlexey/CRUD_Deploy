a5ac8697db5a4ef473273ae46f211e16a02dfd8e
JairAlexey <alexeyrueda823@gmail.com>
2024-10-21 01:39:23 -0500

    Mejorando jwt.js

--- server/src/libs/jwt.js
+++ server/src/libs/jwt.js
@@ -1,16 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { tokenSecret } from '../config.js';
 
-export function createToken(payload) {
+export const createToken = (payload) => {
     return new Promise((resolve, reject) => {
-        jwt.sign(
-            payload,
-            tokenSecret,
-            { expiresIn: '1h' }, 
-            (error, token) => {
-                if (error) reject(error);
+        jwt.sign(payload, tokenSecret, { expiresIn: '1h' }, (error, token) => {
+            if (error) {
+                reject(error);
+            } else {
                 resolve(token);
             }
-        );
+        });
     });
-}
+};
